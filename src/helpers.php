aa682febe7dd44faf8ee9e3830b6ebc819792b26
Aleksander Kalnoy <lazychaser@gmail.com>
2014-01-13 13:20:25 +0400

    Add dock block to helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,36 @@
 <?php namespace Kalnoy\Cruddy;
 
+/**
+ * @param $value
+ *
+ * @return mixed
+ */
 function prettify_string($value) {
 
     return str_replace("_", " ", $value);
 }
 
+/**
+ * Try translate a key.
+ *
+ * @param $key
+ *
+ * @return string
+ */
 function try_trans($key)
 {
     return strpos($key, '.') !== false ? trans($key) : $key;
 }
 
-function extract_list($value, $default = array('*'))
+/**
+ * Explode a list of items separated by comma.
+ *
+ * @param string $value
+ * @param array  $default
+ *
+ * @return array
+ */
+function extract_list($value, $default = ['*'])
 {
     if (empty($value)) return $default;
 

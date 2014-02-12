20977c5a086e0cfdd7526881aa3b00043beceebd
lazychaser <lazychaser@gmail.com>
2014-02-12 13:40:06 +0400

    Translate errors

--- src/lang/en/app.php
+++ src/lang/en/app.php
@@ -1,4 +1,13 @@
-<?php return array(
+<?php return
+[
     'logout' => 'Logout',
     'loading' => 'Please, wait until the app is loaded.',
-);
+
+    'forbidden' =>
+    [
+        'view'   => 'You are not allowed to view the :entity entity.',
+        'create' => 'You are not allowed to create new items of the :entity entity.',
+        'update' => 'You are not allowed to update items of the :entity entity.',
+        'delete' => 'You are not allowed to delete items of the :entity entity.',
+    ],
+];

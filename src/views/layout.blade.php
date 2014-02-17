5e12e68346e64b58b240889d300d0141448b385b
lazychaser <lazychaser@gmail.com>
2014-02-17 14:11:18 +0400

    Fix navbar

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -6,16 +6,18 @@
 </head>
 <body>
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="navbar-header">
-            <a href="{{ url("/") }}" class="navbar-brand">{{ $brand }}</a>
-        </div>
-
-        <div class="navbar-collapse">
-            {{ $cruddy->menu() }}
-
-            <p class="navbar-text navbar-right">
-                <a href="{{ url("logout") }}" type="button" class="navbar-link">@lang('cruddy::app.logout')</a>
-            </p>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="{{ url("/") }}" class="navbar-brand">{{ $brand }}</a>
+            </div>
+            
+            <div class="navbar-collapse">
+                {{ $cruddy->menu() }}
+            
+                <p class="navbar-text navbar-right">
+                    <a href="{{ url("logout") }}" type="button" class="navbar-link">@lang('cruddy::app.logout')</a>
+                </p>
+            </div>
         </div>
     </nav>
 

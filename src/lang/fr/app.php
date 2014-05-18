893e433af311d8e86ba3545fefe7e088e109b6cc
Tony LAURENT <contact@tony-laurent.com>
2014-05-18 15:56:27 +0200

    Add french translation

---
+++ src/lang/fr/app.php
@@ -0,0 +1,13 @@
+<?php return
+[
+    'logout' => 'Déconnexion',
+    'loading' => 'Please, wait until the app is loaded.',
+
+    'forbidden' =>
+    [
+        'view'   => 'You are not allowed to view the :entity entity.',
+        'create' => 'You are not allowed to create new items of the :entity entity.',
+        'update' => 'You are not allowed to update items of the :entity entity.',
+        'delete' => 'You are not allowed to delete items of the :entity entity.',
+    ],
+];
---
+++ src/lang/fr/js.php
@@ -0,0 +1,44 @@
+<?php return
+[
+    'add' => 'Add',
+    'save' => 'Save',
+    'create' => 'Create',
+    'close' => 'Close',
+    'delete' => 'Delete',
+    'copy' => 'Copy',
+    'search' => 'search',
+    'choose' => 'Choose',
+    'not_selected' => 'Not selected',
+    'required' => 'required',
+    'any_value' => 'Any value',
+    'never' => 'Never',
+    'refresh' => 'Refresh',
+    'view_external' => 'View item on main site',
+
+    'no_results' => 'No results',
+    'more' => 'More',
+
+    'success' => 'Success!',
+    'failure' => 'Failure.',
+    'invalid' => 'Validation failed...',
+    'forbidden' => 'Forbidden',
+
+    'prev' => 'Prev',
+    'next' => 'Next',
+
+    'yes' => 'Yes',
+    'no'  => 'No',
+
+    'slug_sync' => 'Synchronize',
+    
+    'markdown_source' => 'Source',
+    'markdown_parsed' => 'Parsed',
+
+    'entity_forbidden' => 'You are not allowed to view this entity.',
+    'entity_not_found' => 'Sorry, but this entity is not found.',
+    'entity_new' => 'New ',
+
+    'confirm_discard' => 'Are you shure you want to discard all changes?',
+    'confirm_abort' => 'Are you shure you want to abort request?',
+    'confirm_delete' => 'Are you shure you want to delete the item? You will not be able to restore it!',
+];

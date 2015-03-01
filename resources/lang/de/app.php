65ceebcc274ebff141015ee6f0ef9ae77327795e
Martin Siedl <martin@siedl.at>
2015-03-01 02:33:28 +0100

    Revert "Revert "add german translation files""
    This reverts commit 59d688a4b7c04118b1fa86226665a59f6b8a65f4.

---
+++ resources/lang/de/app.php
@@ -0,0 +1,10 @@
+<?php return [
+    'loading' => 'Applikation wird geladen ... bitte warten.',
+
+    'forbidden' => [
+        'view'   => 'Sie dürfen Datensätze vom Typ "[:entity]" nicht einsehen',
+        'create' => 'Sie dürfen keine Datensätze vom Typ "[:entity]" anlegen.',
+        'update' => 'Sie dürfen keine Datensätze vom Typ "[:entity]" aktualisieren.',
+        'delete' => 'Sie dürfen keine Datensätze vom Typ "[:entity]" löschen.',
+    ],
+];

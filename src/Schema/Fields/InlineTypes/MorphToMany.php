d76a7ef4fc4a5fe1ac68e6725f95e74ec157614e
Manuel <manuel@retoglobal.es>
2016-02-19 16:10:57 +0100

    Add MorphToMany Relation to Inline Types

---
+++ src/Schema/Fields/InlineTypes/MorphToMany.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kalnoy\Cruddy\Schema\Fields\InlineTypes;
+
+/**
+ * Morph many inline editing.
+ *
+ * @since 1.0.0
+ */
+class MorphToMany extends MorphOne {
+
+    /**
+     * Get whether the relations works with a collection of models.
+     *
+     * @return bool
+     */
+    public function isMultiple()
+    {
+        return true;
+    }
+
+}

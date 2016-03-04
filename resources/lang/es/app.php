9a3e784f097c1c85d6a7665e2e283d98c86342c2
Manuel <manuel@retoglobal.es>
2016-03-04 16:54:21 +0100

    Add spanish translations

---
+++ resources/lang/es/app.php
@@ -0,0 +1,10 @@
+<?php return [
+    'loading' => 'Por favor, espere hasta que la app esté cargada.',
+
+    'forbidden' => [
+        'create' => 'No tienes acceso para crear nuevos items en [:entity].',
+        'read'   => 'No tienes acceso de lectura en [:entity] entity.',
+        'update' => 'No tienes acceso para actualizar items en [:entity] entity.',
+        'delete' => 'No tienes acceso para eliminar items en [:entity] entity.',
+    ],
+];
---
+++ resources/lang/es/js.php
@@ -0,0 +1,70 @@
+<?php return [
+    'add' => 'Añadir',
+    'save' => 'Guardar',
+    'create' => 'Crear',
+    'close' => 'Cerrar',
+    'delete' => 'Eliminar',
+    'restore' => 'Restaurar',
+    'copy' => 'Copiar',
+    'search' => 'buscar',
+    'choose' => 'Elegir',
+    'not_selected' => 'No seleccionado',
+    'required' => 'obligatorio',
+    'any_value' => 'Cualquier valor',
+    'never' => 'Nunca',
+    'refresh' => 'Refrescar',
+    'view_external' => 'Ver elemento en el sitio web',
+
+    'model_refresh' => 'Sincronizar modelo con el servidor',
+    'model_delete' => 'Eliminar del servidor',
+    'model_copy' => 'Copiar modelo',
+    'model_deleted' => 'Modelo destruido.',
+    'model_new_record' => 'Nuevo registro',
+
+    'no_results' => 'Sin resultados',
+    'more' => 'Más',
+
+    'success' => 'Éxito!',
+    'failure' => 'Fallo.',
+    'invalid' => 'Fallo de validación...',
+    'forbidden' => 'Forbidden',
+
+    'form_saved' => 'Guardado!',
+    'form_failed' => 'Hubo un fallo.',
+    'form_invalid' => 'Fallo en la validación...',
+
+    'prev' => 'Ant',
+    'next' => 'Sig',
+
+    'yes' => 'Sí',
+    'no'  => 'No',
+
+    'slug_sync' => 'Sincronizar',
+
+    'markdown_source' => 'Fuente',
+    'markdown_parsed' => 'Parseado',
+
+    'entity_forbidden' => 'No tienes acceso para ver esta entidad.',
+    'entity_not_found' => 'Lo sentimos, pero no se ha encontrado esta entidad.',
+    'entity_new' => 'Nueva ',
+
+    'schema_failed' => 'No se pudo cargar el schema.',
+
+    'confirm_discard' => '¿Estás seguro de querer descartar los cambios?',
+    'confirm_abort' => '¿Estás seguro de querer abortar la solicitud?',
+    'confirm_delete' => '¿Estás seguro de querer eliminar el item? ¡No podrás restaurarlo!',
+
+    'onclose_discard' => 'Se perderán todos los cambios.',
+    'onclose_abort' => 'La solicitud al servidor se abortará.',
+
+    'filter_apply' => 'Aplicar',
+    'filter_reset' => 'Reiniciar',
+
+    'reset' => 'Reiniciar',
+    'edit' => 'Editar',
+    'find' => 'Encontrar',
+    'list_show' => 'Mostrar lista',
+
+    'action_applied' => 'Acción ejecutada con éxito.',
+    'action_failed' => 'Acción falló',
+];
